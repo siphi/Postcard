9f284ae64c6408c81b0fdecac18b82c6705d0a63
Philip Silvestri <philip@silvestri.tv>
2014-09-28 10:54:09 -0400

    Saving code after First Challenge

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+
     @IBOutlet weak var mailButton: UIButton!
     
     
@@ -27,15 +30,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
-        //Some sort or comment
+        //Instructions Messages and label display
+
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
-    
+        
+        enterNameTextField.text = ""
+        enterMessageTextField.resignFirstResponder()
+        
         mailButton.setTitle("Mail Sent Yo!", forState: UIControlState.Normal)
+        
+        
     }
 
 }
